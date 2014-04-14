f435172879d1b97538c13452f8c6e77dadc1fefd
Aleksey <aragorn_108@mail.ru>
2014-04-14 15:02:09 +0900

    Update HeroOne.cs

--- Scripts/HeroOne.cs
+++ Scripts/HeroOne.cs
@@ -2,24 +2,6 @@
 using System.Collections;
 
 public class HeroOne :  IJudje{
-	public HeroOne(){
-		MaxHealt = 100;
-		MaxMana = 200;
-		Mana = MaxMana;
-		Health = MaxHealt;
-		GStrength = 5;
-		Name = "Саня";
-		GEndurance = 4;
-		GIntelligence = 8;
-		GAgility = 6;
-		GMagic = 6;
-		GLucky = 5;
-		ActionPoint = 3;
-		Level = 1;
-		ToLevelUp = 500;
-		CurrentExp = 0;
-		ActionPoint = 3;
-	}
 	public void Display(){
 		Debug.Log(" My Name = "  + Name);
 	}

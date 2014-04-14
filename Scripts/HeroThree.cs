c856c7e2fde7db5899b1179a8cb444fc1efff029
Aleksey <aragorn_108@mail.ru>
2014-04-14 15:02:23 +0900

    Update HeroThree.cs

--- Scripts/HeroThree.cs
+++ Scripts/HeroThree.cs
@@ -2,26 +2,6 @@
 using System.Collections;
 
 public class HeroThree : Characters, IJudje {
-	public HeroThree(){
-		GStrength = 10;
-		GIntelligence = 2;
-		GAgility = 4;
-		GLucky = 5;
-		GEndurance = 4;
-		GMagic = 5;
-		Name = "Лёша";
-		Health = 100;
-		MaxHealt = 100;
-		Mana = 200;
-		MaxMana = 200;
-		Level = 1;
-		CurrentExp = 0;
-		ToLevelUp = 500;
-		Armor = null;
-		Weapon = null;
-		Defend = false;
-		ActionPoint = 3;
-	}
 	public void Display(){
 		Debug.Log(" My Name = "  + Name);
 	}

9f0d3b556091bff7946bd8745ba374883e30f132
Aleksey <aragorn_108@mail.ru>
2014-04-14 15:01:56 +0900

    Update EnemyTwo.cs

--- Scripts/EnemyTwo.cs
+++ Scripts/EnemyTwo.cs
@@ -2,20 +2,6 @@
 using System.Collections;
 
 public class EnemyTwo : IJudje {
-	void Start(){
-		MaxHealt = 100;
-		MaxMana = 200;
-		Mana = MaxMana;
-		Health = MaxHealt;
-		GStrength = 5;
-		Name = "Goblin2";
-		GEndurance = 4;
-		GIntelligence = 8;
-		GAgility = 6;
-		GMagic = 6;
-		GLucky = 5;
-		ActionPoint = 3;
-	}
 	public void Display(){
 		Debug.Log(" My Name = "  + Name);
 	}

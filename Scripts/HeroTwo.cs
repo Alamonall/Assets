17d2a5888d5f8fc0c8bb460630f4b6f36b712b43
Aleksey <aragorn_108@mail.ru>
2014-04-14 15:02:39 +0900

    Update HeroTwo.cs

--- Scripts/HeroTwo.cs
+++ Scripts/HeroTwo.cs
@@ -1,27 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HeroTwo : Characters, IJudje{
-	public HeroTwo(){
-		GStrength = 10;
-		GIntelligence = 2;
-		GAgility = 4;
-		GEndurance = 4;
-		GMagic = 5;
-		GLucky = 5;
-		Name = "Лена";
-		Health = 100;
-		MaxHealt = 100;
-		Mana = 200;
-		MaxMana = 200;
-		Level = 1;
-		CurrentExp = 0;
-		ToLevelUp = 500;
-		Armor = null;
-		Weapon = null;
-		Defend = false;
-		ActionPoint = 4;
-	}
+public class HeroTwo : IJudje{
 	public void Display(){
 		Debug.Log(" My Name = "  + Name);
 	}

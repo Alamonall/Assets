904c1acc16bb949470a602cf7e955e39b66c6c56
Aleksey <aragorn_108@mail.ru>
2014-05-17 19:50:08 +0900

    Криво ИИ
    Написал кривой ИИ

---
+++ Scripts/TestingGUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestingGUI : MonoBehaviour {
+	public Texture2D iCon;
+	public GUIStyle MyStule;
+	void OnGUI () {
+		GUI.BeginGroup(new Rect(Screen.width/2, Screen.height/2,600,700),MyStule);
+			GUI.Box (new Rect(10,10,400, 500),iCon);
+		GUI.EndGroup();
+	}
+}
